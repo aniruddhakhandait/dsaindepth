9697bd5257ec902203b8bafa554ca3af9d3aca95
Aniruddha khandait <samyakkhandait@gmail.com>
2025-11-26 17:09:09 +0530

    adding 7th file in medium folder

---
+++ array/medium/7.groupanagram.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    unordered_map<string, vector<string>> mp;
+
+    for (string s : strs) {
+        string key = s;
+        sort(key.begin(), key.end());  // signature
+        mp[key].push_back(s);
+    }
+
+    vector<vector<string>> result;
+    for (auto &p : mp) {
+        result.push_back(p.second);
+    }
+    return result;
+}
+
+int main() {
+    vector<string> strs = {"eat","tea","tan","ate","nat","bat"};
+    vector<vector<string>> ans = groupAnagrams(strs);
+
+    for (auto& group : ans) {
+        cout << "[ ";
+        for (auto& w : group) cout << w << " ";
+        cout << "]\n";
+    }
+}
