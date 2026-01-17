2ef58e50d1cf589a8af5cb836b6743af50f54f8b
Aniruddha khandait <samyakkhandait@gmail.com>
2026-01-17 17:00:23 +0530

    add hard folder and first file init

---
+++ dynamic programming/hard/1.longestparentheses.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int longestValidParentheses(string s) {
+    stack<int> st;
+    st.push(-1);  // base index
+    int maxLen = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '(') {
+            st.push(i);
+        } else {
+            st.pop();
+            if (st.empty()) {
+                st.push(i);  // reset base
+            } else {
+                maxLen = max(maxLen, i - st.top());
+            }
+        }
+    }
+    return maxLen;
+}
+
+int main() {
+    string s = ")()())";
+    cout << "Longest Valid Parentheses Length: "
+         << longestValidParentheses(s) << endl;
+    return 0;
+}
